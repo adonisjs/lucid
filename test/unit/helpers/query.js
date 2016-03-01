79d9cc9aa17bc06c802fdd96084fc3a94ba75fa4
Harminder Virk <virk.officials@gmail.com>
2016-03-01 21:41:07 +0530

    refactor(tests): integerate postgres test
    add postgres test and fix formatting errors caused by postgres

--- test/unit/helpers/query.js
+++ test/unit/helpers/query.js
@@ -6,11 +6,22 @@
  * MIT Licensed
 */
 
+const _ = require('lodash')
+
 module.exports = {
   formatQuery: function (query) {
     if(process.env.DB === 'mysql') {
       return query.replace(/"/g, '`')
     }
     return query
+  },
+
+  formatBindings: function (bindings) {
+    if(process.env.DB === 'pg') {
+      return _.map(bindings, function (binding) {
+        return binding.toString()
+      })
+    }
+    return bindings
   }
 }
