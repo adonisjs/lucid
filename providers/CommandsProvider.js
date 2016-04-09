c5bddf029cdf026d3511b87b269c8a1a6db33847
Harminder Virk <virk.officials@gmail.com>
2016-04-09 11:11:22 +0530

    refactor(commands): Rename commands by prefixing Migration:

--- providers/CommandsProvider.js
+++ providers/CommandsProvider.js
@@ -15,18 +15,25 @@ class CommandsProvider extends ServiceProvider {
 
   constructor () {
     super()
-    this.commands = ['Run', 'Rollback', 'Refresh', 'Reset', 'Seed', 'Status']
+    this.commands = ['Run', 'Rollback', 'Refresh', 'Reset', 'Status']
   }
 
   * register () {
     this.commands.forEach((command) => {
-      this.app.bind(`Adonis/Commands/${command}`, function (app) {
+      this.app.bind(`Adonis/Commands/Migration:${command}`, function (app) {
         const CommandFile = require(`../src/Commands/${command}`)
         const Migrations = app.use('Adonis/Src/Migrations')
         const Helpers = app.use('Adonis/Src/Helpers')
         const Seeder = app.use('Adonis/Src/Seeder')
         return new CommandFile(Helpers, Migrations, Seeder)
       })
+    })
+    this.app.bind('Adonis/Commands/DB:Seed', function (app) {
+      const SeedCommand = require('../src/Commands/Seed')
+      const Migrations = app.use('Adonis/Src/Migrations')
+      const Helpers = app.use('Adonis/Src/Helpers')
+      const Seeder = app.use('Adonis/Src/Seeder')
+      return new SeedCommand(Helpers, Migrations, Seeder)
     })
   }
 }
