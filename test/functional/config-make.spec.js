98a318ac13aa72a7a5da6f30ccdabf9bd9a2cce0
Harminder Virk <virk.officials@gmail.com>
2017-07-30 14:09:39 +0530

    feat(commands): add config:database command

--- test/functional/config-make.spec.js
+++ test/functional/config-make.spec.js
@@ -15,9 +15,9 @@ const fs = require('fs-extra')
 const path = require('path')
 const { ioc } = require('@adonisjs/fold')
 const { setupResolver, Helpers } = require('@adonisjs/sink')
-const MigrationMake = require('../../commands/MigrationMake')
+const MakeConfig = require('../../commands/MakeConfig')
 
-test.group('Migration Make', (group) => {
+test.group('Make Config', (group) => {
   group.before(async () => {
     ioc.bind('Adonis/Src/Helpers', () => {
       return new Helpers(path.join(__dirname))
@@ -27,7 +27,7 @@ test.group('Migration Make', (group) => {
 
   group.after(async () => {
     try {
-      await fs.remove(path.join(__dirname, 'database'))
+      await fs.remove(path.join(__dirname, 'config'))
     } catch (error) {
       if (process.platform !== 'win32' || error.code !== 'EBUSY') {
         throw error
@@ -35,10 +35,16 @@ test.group('Migration Make', (group) => {
     }
   }).timeout(0)
 
-  test('create migration file', async (assert) => {
-    ace.addCommand(MigrationMake)
-    const result = await ace.call('make:migration', { name: 'Users' }, { action: 'create' })
+  test('create config file', async (assert) => {
+    ace.addCommand(MakeConfig)
+    const result = await ace.call('config:database')
     const exists = await fs.pathExists(result)
     assert.isTrue(exists)
+  })
+
+  test('echo config file to console', async (assert) => {
+    ace.addCommand(MakeConfig)
+    const result = await ace.call('config:database', {}, { echo: true })
+    assert.equal(result, 'echoed')
   })
 })
