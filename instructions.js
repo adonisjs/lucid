993eebf0da4aa5e380bd3821d7eea316545ba15b
Harminder Virk <virk.officials@gmail.com>
2017-08-01 17:53:53 +0530

    refactor(instructions): log config file has been created

--- instructions.js
+++ instructions.js
@@ -14,6 +14,7 @@ const path = require('path')
 module.exports = async function (cli) {
   try {
     await cli.makeConfig('database.js', path.join(__dirname, './templates/config.mustache'))
+    cli.command.completed('create', 'config/database.js')
   } catch (error) {
     // ignore errors
   }
