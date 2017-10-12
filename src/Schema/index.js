c917d036c162c76c83bfb0bfe52a82bf17efe0a3
Romain Lanz <romain.lanz@slynova.ch>
2017-10-12 16:08:26 +0200

    fix(schema): add alias to createTableIfNotExists
    Closes https://github.com/adonisjs/adonis-lucid/issues/189

--- src/Schema/index.js
+++ src/Schema/index.js
@@ -287,6 +287,15 @@ class Schema {
   }
 
   /**
+   * Alias for @ref('Schema.createTableIfNotExists')
+   *
+   * @method dropIfExists
+   */
+  createIfNotExists (...args) {
+    return this.createTableIfNotExists(...args)
+  }
+
+  /**
    * Alias for @ref('Schema.dropTable')
    *
    * @method drop
