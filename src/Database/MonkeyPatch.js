b0b11f57bd16f1834640d7eb32383b890395d96c
Harminder Virk <virk.officials@gmail.com>
2017-11-06 14:18:48 +0530

    refactor(database): refactor code style

--- src/Database/MonkeyPatch.js
+++ src/Database/MonkeyPatch.js
@@ -131,13 +131,38 @@ KnexQueryBuilder.prototype.paginate = async function (page = 2, perPage = 20) {
   }
 }
 
+/**
+ * Generates an aggregate function, that returns the aggregated result
+ * as a number.
+ *
+ * @method generateAggregate
+ *
+ * @param  {String}          aggregateOp
+ * @param  {String}          defaultColumnName
+ *
+ * @return {Number}
+ *
+ * @example
+ * ```js
+ * generateAggregate('count')
+ * ```
+ */
 function generateAggregate (aggregateOp, defaultColumnName = undefined) {
-  let funcName = 'get' + aggregateOp.charAt(0).toUpperCase() + aggregateOp.slice(1)
+  let funcName = `get${_.upperFirst(aggregateOp)}`
+
+  /**
+   * Do not re-add the method if exists
+   */
+  if (KnexQueryBuilder.prototype[funcName]) {
+    return
+  }
+
   KnexQueryBuilder.prototype[funcName] = async function (columnName = defaultColumnName) {
-    if (!columnName) throw new Error(`'${funcName}' requires a column name.`)
-    let wrapper = new this.constructor(this.client)
-    wrapper.from(this.as('__lucid'))[aggregateOp](`${columnName} as __lucid_aggregate`)
-    let results = await wrapper
+    if (!columnName) {
+      throw new Error(`'${funcName}' requires a column name.`)
+    }
+
+    const results = await this.clone().from(this.as('__lucid'))[aggregateOp](`${columnName} as __lucid_aggregate`)
     return results[0].__lucid_aggregate
   }
 }
