2cab307d504b39efae89caecdc02eddb6f6d7f98
Harminder <virk.officials@gmail.com>
2018-02-23 14:56:46 +0530

    style(linter): lint code as per standard 10

--- src/Factory/DatabaseFactory.js
+++ src/Factory/DatabaseFactory.js
@@ -39,9 +39,9 @@ class DatabaseFactory {
    * @private
    */
   _getQueryBuilder () {
-    return (this._connection
+    return this._connection
       ? ioc.use('Adonis/Src/Database').connection(this._connection)
-      : ioc.use('Adonis/Src/Database')).table(this.tableName)
+      : ioc.use('Adonis/Src/Database')
   }
 
   /**
@@ -169,7 +169,7 @@ class DatabaseFactory {
    */
   async create (data = {}, index = 0) {
     const attributes = await this.make(data, index)
-    const query = this._getQueryBuilder()
+    const query = this._getQueryBuilder().table(this.tableName)
 
     if (this._returningColumn) {
       query.returning(this._returningColumn)
@@ -203,7 +203,7 @@ class DatabaseFactory {
    * @return {Number}
    */
   async reset () {
-    return this._getQueryBuilder().truncate()
+    return this._getQueryBuilder().table(this.tableName).truncate()
   }
 }
 
