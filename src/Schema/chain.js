556314b68dfbd26e2ea8ecac3a3772d3f4e4fad8
nurbol <alpysbayev@mail.ru>
2018-06-10 18:52:46 +0600

    changed "target" option in tsconfig.json to "es6"

--- src/Schema/chain.js
+++ src/Schema/chain.js
@@ -7,33 +7,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 /**
  * adonis-schema
  *
@@ -42,9 +15,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
 */
-var GE = require('@adonisjs/generic-exceptions');
-var SchemaChain = /** @class */ (function () {
-    function SchemaChain() {
+const GE = require('@adonisjs/generic-exceptions');
+class SchemaChain {
+    constructor() {
         this._deferredActions = [];
         this._scheduleFn = null;
     }
@@ -57,10 +30,10 @@ var SchemaChain = /** @class */ (function () {
      *
      * @chainable
      */
-    SchemaChain.prototype.withSchema = function (schema) {
+    withSchema(schema) {
         this._deferredActions.push({ name: 'withSchema', args: [schema] });
         return this;
-    };
+    }
     /**
      * Create a extension.
      *
@@ -72,9 +45,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.createExtension = function (extensionName) {
+    createExtension(extensionName) {
         this._deferredActions.push({ name: 'createExtension', args: [extensionName] });
-    };
+    }
     /**
      * Create a extension if not already exists.
      *
@@ -86,9 +59,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.createExtensionIfNotExists = function (extensionName) {
+    createExtensionIfNotExists(extensionName) {
         this._deferredActions.push({ name: 'createExtensionIfNotExists', args: [extensionName] });
-    };
+    }
     /**
      * Create a new table.
      *
@@ -101,9 +74,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.createTable = function (tableName, callback) {
+    createTable(tableName, callback) {
         this._deferredActions.push({ name: 'createTable', args: [tableName, callback] });
-    };
+    }
     /**
      * Create a new table if not already exists.
      *
@@ -116,9 +89,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.createTableIfNotExists = function (tableName, callback) {
+    createTableIfNotExists(tableName, callback) {
         this._deferredActions.push({ name: 'createTableIfNotExists', args: [tableName, callback] });
-    };
+    }
     /**
      * Rename existing table.
      *
@@ -131,9 +104,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.renameTable = function (fromTable, toTable) {
+    renameTable(fromTable, toTable) {
         this._deferredActions.push({ name: 'renameTable', args: [fromTable, toTable] });
-    };
+    }
     /**
      * Drop existing extension.
      *
@@ -145,9 +118,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.dropExtension = function (extensionName) {
+    dropExtension(extensionName) {
         this._deferredActions.push({ name: 'dropExtension', args: [extensionName] });
-    };
+    }
     /**
      * Drop extension only if it exists.
      *
@@ -159,9 +132,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.dropExtensionIfExists = function (extensionName) {
+    dropExtensionIfExists(extensionName) {
         this._deferredActions.push({ name: 'dropExtensionIfExists', args: [extensionName] });
-    };
+    }
     /**
      * Drop existing table.
      *
@@ -173,9 +146,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.dropTable = function (tableName) {
+    dropTable(tableName) {
         this._deferredActions.push({ name: 'dropTable', args: [tableName] });
-    };
+    }
     /**
      * Drop table only if it exists.
      *
@@ -187,9 +160,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.dropTableIfExists = function (tableName) {
+    dropTableIfExists(tableName) {
         this._deferredActions.push({ name: 'dropTableIfExists', args: [tableName] });
-    };
+    }
     /**
      * Select table for altering it.
      *
@@ -202,9 +175,9 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.table = function (tableName, callback) {
+    table(tableName, callback) {
         this._deferredActions.push({ name: 'table', args: [tableName, callback] });
-    };
+    }
     /* istanbul ignore next */
     /**
      * Run a raw SQL statement
@@ -217,10 +190,10 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.raw = function (statement) {
+    raw(statement) {
         this._deferredActions.push({ name: 'raw', args: [statement] });
         return this;
-    };
+    }
     /**
      * Schedule a method to be executed in sequence with migrations
      *
@@ -230,60 +203,60 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.schedule = function (fn) {
+    schedule(fn) {
         if (typeof (fn) !== 'function') {
-            throw GE.InvalidArgumentException.invalidParameter("this.schedule expects 1st argument to be a function");
+            throw GE.InvalidArgumentException.invalidParameter(`this.schedule expects 1st argument to be a function`);
         }
         this._scheduleFn = fn;
-    };
+    }
     /**
      * Alias for @ref('Schema.table')
      *
      * @method alter
      */
-    SchemaChain.prototype.alter = function (tableName, callback) {
+    alter(tableName, callback) {
         return this.table(tableName, callback);
-    };
+    }
     /**
      * Alias for @ref('Schema.createTable')
      *
      * @method create
      */
-    SchemaChain.prototype.create = function (tableName, callback) {
+    create(tableName, callback) {
         return this.createTable(tableName, callback);
-    };
+    }
     /**
      * Alias for @ref('Schema.createTableIfNotExists')
      *
      * @method createIfNotExists
      */
-    SchemaChain.prototype.createIfNotExists = function (tableName, callback) {
+    createIfNotExists(tableName, callback) {
         return this.createTableIfNotExists(tableName, callback);
-    };
+    }
     /**
      * Alias for @ref('Schema.dropTable')
      *
      * @method drop
      */
-    SchemaChain.prototype.drop = function (tableName) {
+    drop(tableName) {
         return this.dropTable(tableName);
-    };
+    }
     /**
      * Alias for @ref('Schema.dropTableIfExists')
      *
      * @method dropIfExists
      */
-    SchemaChain.prototype.dropIfExists = function (tableName) {
+    dropIfExists(tableName) {
         return this.dropTableIfExists(tableName);
-    };
+    }
     /**
      * Alias for @ref('Schema.renameTable')
      *
      * @method rename
      */
-    SchemaChain.prototype.rename = function (fromTable, toTable) {
+    rename(fromTable, toTable) {
         return this.renameTable(fromTable, toTable);
-    };
+    }
     /**
      * Returns the SQL query for all the actions.
      *
@@ -291,10 +264,10 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {String}
      */
-    SchemaChain.prototype.toString = function (schema) {
-        this._deferredActions.forEach(function (action) { return (schema[action.name].apply(schema, action.args)); });
+    toString(schema) {
+        this._deferredActions.forEach((action) => (schema[action.name](...action.args)));
         return schema.toString();
-    };
+    }
     /**
      * Executes the deferred actions on a single chain. This method will
      * rollback the trx on error.
@@ -305,49 +278,37 @@ var SchemaChain = /** @class */ (function () {
      *
      * @return {void}
      */
-    SchemaChain.prototype.execute = function (trx) {
-        return __awaiter(this, void 0, void 0, function () {
-            var error_1, schema, error_2;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        if (!(typeof (this._scheduleFn) === 'function')) return [3 /*break*/, 5];
-                        _a.label = 1;
-                    case 1:
-                        _a.trys.push([1, 3, , 4]);
-                        return [4 /*yield*/, this._scheduleFn(trx)];
-                    case 2:
-                        _a.sent();
-                        return [3 /*break*/, 4];
-                    case 3:
-                        error_1 = _a.sent();
-                        trx.rollback();
-                        throw error_1;
-                    case 4: return [2 /*return*/];
-                    case 5:
-                        schema = trx.schema;
-                        /**
-                         * Looping over all the deferred actions
-                         */
-                        this._deferredActions.forEach(function (action) { return (schema[action.name].apply(schema, action.args)); });
-                        _a.label = 6;
-                    case 6:
-                        _a.trys.push([6, 8, , 9]);
-                        return [4 /*yield*/, schema];
-                    case 7:
-                        _a.sent();
-                        this._deferredActions = [];
-                        return [3 /*break*/, 9];
-                    case 8:
-                        error_2 = _a.sent();
-                        trx.rollback();
-                        throw error_2;
-                    case 9: return [2 /*return*/];
+    execute(trx) {
+        return __awaiter(this, void 0, void 0, function* () {
+            /**
+             * If schedule fn is defined, then execute it. Within a chain a user
+             * can never have `schedule` and `deferredActions` together.
+             */
+            if (typeof (this._scheduleFn) === 'function') {
+                try {
+                    yield this._scheduleFn(trx);
                 }
-            });
+                catch (error) {
+                    trx.rollback();
+                    throw error;
+                }
+                return;
+            }
+            const schema = trx.schema;
+            /**
+             * Looping over all the deferred actions
+             */
+            this._deferredActions.forEach((action) => (schema[action.name](...action.args)));
+            try {
+                yield schema;
+                this._deferredActions = [];
+            }
+            catch (error) {
+                trx.rollback();
+                throw error;
+            }
         });
-    };
-    return SchemaChain;
-}());
+    }
+}
 module.exports = SchemaChain;
 //# sourceMappingURL=chain.js.map
--- src/Schema/index.js
+++ src/Schema/index.js
@@ -7,34 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var SchemaChain = require("./chain");
+const SchemaChain = require("./chain");
 /*
  * adonis-lucid
  *
@@ -55,53 +28,41 @@ var SchemaChain = require("./chain");
  * @class Schema
  * @constructor
  */
-var Schema = /** @class */ (function () {
-    function Schema(Database) {
+class Schema {
+    constructor(Database) {
         this.db = Database.connection(this.constructor.connection);
         this._chains = [];
     }
-    Object.defineProperty(Schema, "connection", {
-        /**
-         * Connection to be used for schema
-         *
-         * @attribute connection
-         *
-         * @return {String}
-         */
-        get: function () {
-            return '';
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Schema.prototype, "schema", {
-        /**
-         * The schema instance of knex
-         *
-         * @attribute schema
-         *
-         * @return {Object}
-         */
-        get: function () {
-            return this.db.schema;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Schema.prototype, "fn", {
-        /**
-         * Access to db fn
-         *
-         * @attribute fn
-         *
-         * @return {Object}
-         */
-        get: function () {
-            return this.db.fn;
-        },
-        enumerable: true,
-        configurable: true
-    });
+    /**
+     * Connection to be used for schema
+     *
+     * @attribute connection
+     *
+     * @return {String}
+     */
+    static get connection() {
+        return '';
+    }
+    /**
+     * The schema instance of knex
+     *
+     * @attribute schema
+     *
+     * @return {Object}
+     */
+    get schema() {
+        return this.db.schema;
+    }
+    /**
+     * Access to db fn
+     *
+     * @attribute fn
+     *
+     * @return {Object}
+     */
+    get fn() {
+        return this.db.fn;
+    }
     /**
      * Returns a boolean indicating if a table
      * already exists or not
@@ -112,9 +73,9 @@ var Schema = /** @class */ (function () {
      *
      * @return {Boolean}
      */
-    Schema.prototype.hasTable = function (tableName) {
+    hasTable(tableName) {
         return this.schema.hasTable(tableName);
-    };
+    }
     /* istanbul ignore next */
     /**
      * Returns a boolean indicating if a column exists
@@ -127,9 +88,9 @@ var Schema = /** @class */ (function () {
      *
      * @return {Boolean}
      */
-    Schema.prototype.hasColumn = function (tableName, columnName) {
+    hasColumn(tableName, columnName) {
         return this.schema.hasColumn(tableName, columnName);
-    };
+    }
     /**
      * Execute deferred actions in sequence. All the actions will be
      * wrapped inside a transaction, which get's rolled back on
@@ -141,65 +102,43 @@ var Schema = /** @class */ (function () {
      *
      * @return {Array}
      */
-    Schema.prototype.executeActions = function (getSql) {
-        if (getSql === void 0) { getSql = false; }
-        return __awaiter(this, void 0, void 0, function () {
-            var _this = this;
-            var trx, _i, _a, chain;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0:
-                        /**
-                         * Returns SQL array over executing the actions
-                         */
-                        if (getSql) {
-                            return [2 /*return*/, this._chains.map(function (chain) { return chain.toString(_this.schema); })];
-                        }
-                        return [4 /*yield*/, this.db.beginTransaction()
-                            /**
-                             * Execute all the chains
-                             */
-                        ];
-                    case 1:
-                        trx = _b.sent();
-                        _i = 0, _a = this._chains;
-                        _b.label = 2;
-                    case 2:
-                        if (!(_i < _a.length)) return [3 /*break*/, 5];
-                        chain = _a[_i];
-                        return [4 /*yield*/, chain.execute(trx)];
-                    case 3:
-                        _b.sent();
-                        _b.label = 4;
-                    case 4:
-                        _i++;
-                        return [3 /*break*/, 2];
-                    case 5:
-                        /**
-                         * Finally commit the transaction
-                         */
-                        trx.commit();
-                        return [2 /*return*/, []];
-                }
-            });
+    executeActions(getSql = false) {
+        return __awaiter(this, void 0, void 0, function* () {
+            /**
+             * Returns SQL array over executing the actions
+             */
+            if (getSql) {
+                return this._chains.map((chain) => chain.toString(this.schema));
+            }
+            /**
+             * We need one transaction per class, so we start it here, instead of
+             * doing it inside the `for of` loop.
+             */
+            const trx = yield this.db.beginTransaction();
+            /**
+             * Execute all the chains
+             */
+            for (let chain of this._chains) {
+                yield chain.execute(trx);
+            }
+            /**
+             * Finally commit the transaction
+             */
+            trx.commit();
+            return [];
         });
-    };
-    return Schema;
-}());
+    }
+}
 /**
  * Copying all the chain method to the Schema prototype.
  */
 Object
     .getOwnPropertyNames(SchemaChain.prototype)
-    .filter(function (method) { return method !== 'constructor'; })
-    .forEach(function (method) {
-    Schema.prototype[method] = function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        var chain = new SchemaChain();
-        chain[method].apply(chain, args);
+    .filter((method) => method !== 'constructor')
+    .forEach((method) => {
+    Schema.prototype[method] = function (...args) {
+        const chain = new SchemaChain();
+        chain[method](...args);
         this._chains.push(chain);
         return chain;
     };
