f5e7fbd98c73e84697ba6c65cf212bd2e29b812b
Harminder virk <virk.officials@gmail.com>
2019-11-26 23:25:28 +0530

    chore: setup providers and commands instructions inside package file

---
+++ commands/index.ts
@@ -0,0 +1,15 @@
+/*
+ * @adonisjs/lucid
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+export default [
+  '@adonisjs/lucid/build/MakeMigration',
+  '@adonisjs/lucid/build/Migrate',
+  '@adonisjs/lucid/build/Rollback',
+  '@adonisjs/lucid/build/Status',
+]
