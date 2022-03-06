bed3cca223b37f1b15f6841a751e637b11176dfc
Harminder Virk <virk.officials@gmail.com>
2022-03-06 10:59:37 +0530

    fix: make assertions more resilient

--- test/orm/model-many-to-many.spec.ts
+++ test/orm/model-many-to-many.spec.ts
@@ -4848,7 +4848,7 @@ test.group('Model | ManyToMany | save', (group) => {
     assert.equal(skillUsers[0].skill_id, skill.id)
     assert.isNotNull(skillUsers[0].created_at)
     assert.isNotNull(skillUsers[0].updated_at)
-    assert.deepEqual(skillUsers[0].created_at, skillUsers[0].updated_at)
+    assert.isAtLeast(skillUsers[0].updated_at, skillUsers[0].created_at)
     assert.isUndefined(user.$trx)
     assert.isUndefined(skill.$trx)
   })
