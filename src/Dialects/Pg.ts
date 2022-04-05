747986a34bf3bec72ef469c5f4e7eb457d96b8fc
Alyx <25337749+Alyxsqrd@users.noreply.github.com>
2022-04-05 08:53:01 -0400

    Use template literals

--- src/Dialects/Pg.ts
+++ src/Dialects/Pg.ts
@@ -89,7 +89,7 @@ export class PgDialect implements DialectContract {
     const tables = await this.getAllTables(schemas)
     if (!tables.length) return
 
-    await this.client.rawQuery('DROP TABLE IF EXISTS "' + tables.join('", "') + '" CASCADE;')
+    await this.client.rawQuery(`DROP TABLE IF EXISTS "${tables.join('", "')}" CASCADE;`)
   }
 
   /**
@@ -99,7 +99,7 @@ export class PgDialect implements DialectContract {
     const views = await this.getAllViews(schemas)
     if (!views.length) return
 
-    await this.client.rawQuery('DROP VIEW IF EXISTS "' + views.join('", "') + '" CASCADE;')
+    await this.client.rawQuery(`DROP VIEW IF EXISTS "${views.join('", "')}" CASCADE;`)
   }
 
   /**
@@ -109,7 +109,7 @@ export class PgDialect implements DialectContract {
     const types = await this.getAllTypes(schemas)
     if (!types.length) return
 
-    await this.client.rawQuery('DROP TYPE IF EXISTS "' + types.join('", "') + '" CASCADE;')
+    await this.client.rawQuery(`DROP TYPE IF EXISTS "${types.join('", "')}" CASCADE;`)
   }
 
   /**
