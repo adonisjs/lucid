170f2a826e9fa97068ea02b8636ea155e07cc10d
Harminder Virk <virk.officials@gmail.com>
2022-08-26 15:12:45 +0530

    test: increase docker wait-for-it timeout time

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/wait-for-it.sh mysql_legacy:3306 && /wait-for-it.sh mysql_replica_1:3306 && /wait-for-it.sh mysql_replica_2:3306 && /wait-for-it.sh mysql:3306 && /wait-for-it.sh pg:5432 && /wait-for-it.sh microsoftsql:1433 && npm run test:docker
+/wait-for-it.sh -t 30 mysql_legacy:3306 && /wait-for-it.sh  -t 30 mysql_replica_1:3306 && /wait-for-it.sh -t 30 mysql_replica_2:3306 && /wait-for-it.sh -t 30 mysql:3306 && /wait-for-it.sh -t 30 pg:5432 && /wait-for-it.sh -t 30 microsoftsql:1433 && npm run test:docker
