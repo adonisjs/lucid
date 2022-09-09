5a29d976345208391a7c7f95b012c05186b188e6
Julien Ripouteau <julien@ripouteau.com>
2022-09-09 18:36:29 +0200

    refactor: new ci pipeline (#878)

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/wait-for-it.sh -t 30 mysql_legacy:3306 && /wait-for-it.sh  -t 30 mysql_replica_1:3306 && /wait-for-it.sh -t 30 mysql_replica_2:3306 && /wait-for-it.sh -t 30 mysql:3306 && /wait-for-it.sh -t 30 pg:5432 && /wait-for-it.sh -t 30 microsoftsql:1433 && npm run test:docker
