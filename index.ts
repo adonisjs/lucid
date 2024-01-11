ae090bd093ff7ce41d05054284e5b3edd197f54d
Harminder Virk <virk.officials@gmail.com>
2024-01-11 07:31:51 +0530

    refactor: export stubsRoot

--- index.ts
+++ index.ts
@@ -9,4 +9,5 @@
 
 export * as errors from './src/errors.js'
 export { configure } from './configure.js'
+export { stubsRoot } from './stubs/main.js'
 export { defineConfig } from './src/define_config.js'
