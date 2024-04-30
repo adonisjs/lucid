f8bc8eb3207533f4ef7eb367c2c76e2ff99c5d7d
Ayush Gupta <er.ayush2612@gmail.com>
2024-04-30 19:50:25 +0530

    feat: Export ModelPaginator Class

--- src/orm/main.ts
+++ src/orm/main.ts
@@ -12,4 +12,5 @@ export * from './decorators/date.js'
 export * from './decorators/date_time.js'
 export { BaseModel, scope } from './base_model/index.js'
 export { ModelQueryBuilder } from './query_builder/index.js'
+export { ModelPaginator } from './paginator/index.js';
 export { SnakeCaseNamingStrategy } from './naming_strategies/snake_case.js'
