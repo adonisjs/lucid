dae77a2395443e3fe9cc78eecc36b874dab709c8
Romain Lanz <romain.lanz@pm.me>
2025-03-23 10:06:06 +0100

    fix(orm): add missing type for new quiet variant

--- src/types/model.ts
+++ src/types/model.ts
@@ -978,9 +978,27 @@ export interface LucidModel {
   ): Promise<InstanceType<T>>
 
   /**
+   * Same as [[BaseModel.create]] but without invoking hooks
+   */
+  createQuietly<T extends LucidModel>(
+    this: T,
+    values: Partial<ModelAttributes<InstanceType<T>>>,
+    options?: ModelAssignOptions
+  ): Promise<InstanceType<T>>
+
+  /**
    * Create many of model instances
    */
   createMany<T extends LucidModel>(
+    this: T,
+    values: Partial<ModelAttributes<InstanceType<T>>>[],
+    options?: ModelAssignOptions
+  ): Promise<InstanceType<T>[]>
+
+  /**
+   * Same as [[BaseModel.createMany]] but without invoking hooks
+   */
+  createManyQuietly<T extends LucidModel>(
     this: T,
     values: Partial<ModelAttributes<InstanceType<T>>>[],
     options?: ModelAssignOptions
