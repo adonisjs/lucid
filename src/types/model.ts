d0ec988802f997b3f12470986dbccb15e5aaf166
Adrien Morel <adrien.morel@crosscall.com>
2026-04-21 15:27:42 +0200

    fix: exclude $columns from ModelAttributes inferred type

--- src/types/model.ts
+++ src/types/model.ts
@@ -93,12 +93,11 @@ export type OptionalTypedDecorator<PropType> = <
 /**
  * A complex type that filters out functions and relationships from the
  * model attributes and consider all other properties as database
- * columns. Alternatively, the user can self define a `$columns`
- * property.
+ * columns.
  */
 export type ModelAttributes<Model extends LucidRow> = {
   [Filtered in {
-    [P in keyof Model]: P extends keyof LucidRow | 'serializeExtras'
+    [P in keyof Model]: P extends keyof LucidRow | 'serializeExtras' | '$columns'
       ? never
       : Model[P] extends Function | ModelRelationTypes
         ? never
